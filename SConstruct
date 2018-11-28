f3a92756ad8c0f208f745dbde24ca8306d8d4d94
Ole Hansen <ole@jlab.org>
2018-11-28 14:56:52 -0500

    SCons 3.0 compatibility fixes
    SCons 3.0 uses python3.  Python 3 doesn't like some things.
     1.  print statements without stuff being printed in parens
     2.  The old octal number format (Use 0o prefix instead of 0)
     3.  Tab characters in the indentation.
    
    (adoped from commit 8daaeff8e0d009a790374d195b12ad93fe8ffa85)

--- SConstruct
+++ SConstruct
@@ -22,7 +22,7 @@ from subprocess import Popen, PIPE
 import coda
 
 # Created files & dirs will have this permission
-os.umask(002)
+os.umask(0o2)
 
 # Software version
 versionMajor = '5'
@@ -41,13 +41,13 @@ ldLibPath = os.getenv('LD_LIBRARY_PATH', '')
 
 if path == '':
     print
-    print "Error: set PATH environmental variable"
+    print ("Error: set PATH environmental variable")
     print
     raise SystemExit
 
 if ldLibPath == '':
     print
-    print "Warning: LD_LIBRARY_PATH environmental variable not defined"
+    print ("Warning: LD_LIBRARY_PATH environmental variable not defined")
     print
     env = Environment(ENV = {'PATH' : os.environ['PATH']})
 else:
@@ -57,16 +57,16 @@ else:
 ################################
 # 64 or 32 bit operating system?
 ################################
-    
+
 # How many bits is the operating system?
 # For Linux 64 bit x86 machines, the "machine' variable is x86_64,
 # but for Darwin or Solaris there is no obvious check so run
 # a configure-type test.
 is64bits = coda.is64BitMachine(env, platform, machine)
 if is64bits:
-    print "We're on a 64-bit machine"
+    print ("We're on a 64-bit machine")
 else:
-    print "We're on a 32-bit machine"
+    print ("We're on a 32-bit machine")
 
 
 #############################################
@@ -79,13 +79,13 @@ Help('\nlocal scons OPTIONS:\n')
 # debug option
 AddOption('--dbg', dest='ddebug', default=False, action='store_true')
 debug = GetOption('ddebug')
-if debug: print "Enable debugging"
+if debug: print ("Enable debugging")
 Help('--dbg               compile with debug flag\n')
 
 # 32 bit option
 AddOption('--32bits', dest='use32bits', default=False, action='store_true')
 use32bits = GetOption('use32bits')
-if use32bits: print "use 32-bit libs & executables even on 64 bit system"
+if use32bits: print ("use 32-bit libs & executables even on 64 bit system")
 Help('--32bits            compile 32bit libs & executables on 64bit system\n')
 
 # install directory option
@@ -148,7 +148,7 @@ if platform == 'Darwin':
 if is64bits and use32bits:
     osname = osname + '-32'
 
-print "OSNAME =", osname
+print ("OSNAME = "+osname)
 
 # hidden sub directory into which variant builds go
 archDir = '.' + osname + debugSuffix
@@ -174,24 +174,24 @@ if (incdir == None):
 if 'install' in COMMAND_LINE_TARGETS:
     # Determine installation directories
     installDirs = coda.getInstallationDirs(osname, prefix, incdir, libdir, bindir)
-    
+
     mainInstallDir    = installDirs[0]
     osDir             = installDirs[1]
     incInstallDir     = installDirs[2]
     archIncInstallDir = installDirs[3]
     libInstallDir     = installDirs[4]
     binInstallDir     = installDirs[5]
-    
+
     # Create the include directories (make symbolic link if possible)
     coda.makeIncludeDirs(incInstallDir, archIncInstallDir, osDir, archIncLocalLink)
 
-    print 'Main install dir  = ', mainInstallDir
-    print 'bin  install dir  = ', binInstallDir
-    print 'lib  install dir  = ', libInstallDir
-    print 'inc  install dirs = ', incInstallDir, ", ", archIncInstallDir
+    print ('Main install dir  = '+mainInstallDir)
+    print ('bin  install dir  = '+binInstallDir)
+    print ('lib  install dir  = '+libInstallDir)
+    print ('inc  install dirs = '+incInstallDir+", "+archIncInstallDir)
 
 else:
-    print 'No installation being done'
+    print ('No installation being done')
 
 print
 
--- coda.py
+++ coda.py
@@ -58,7 +58,7 @@ def subdirsContaining(root, patterns):
 def CheckHas64Bits(context, flags):
     """Define configure-type test function to
        see if operating system is 64 or 32 bits"""
-       
+
     context.Message( 'Checking for 64/32 bits ...' )
     lastCCFLAGS = context.env['CCFLAGS']
     lastLFLAGS  = context.env['LINKFLAGS']
@@ -95,13 +95,13 @@ def is64BitMachine(env, platform, machine):
         ccflags = ''
         if platform == 'SunOS':
             ccflags = '-xarch=amd64'
-        
+
         # Run the test
         conf = Configure( env, custom_tests = { 'CheckBits' : CheckHas64Bits } )
         ret = conf.CheckBits(ccflags)
         env = conf.Finish()
         if ret < 1:
-            print 'Cannot run test, assume 64 bit system'
+            print ('Cannot run test, assume 64 bit system')
             return True
         elif ret == 64:
             # Test shows 64 bit system'
@@ -142,10 +142,10 @@ def configure32bits(env, use32bits, platform):
 
 def getInstallationDirs(osname, prefix, incdir, libdir, bindir):
     """Determine installation directories."""
-    
-    # Get CODA env variable if any         
+
+    # Get CODA env variable if any
     codaHomeEnv = os.getenv('CODA','')
-    
+
     # The installation directory is the user-specified "prefix"
     # by first choice, "CODA" secondly.
     # Any user specified command line installation path overrides default
@@ -153,14 +153,14 @@ def getInstallationDirs(osname, prefix, incdir, libdir, bindir):
         # prefix not defined try CODA env var
         if codaHomeEnv == "":
             if (incdir == None) or (libdir == None) or (bindir == None):
-                print "\nNeed to define CODA, or use the --prefix option,"
-                print "or all the --incdir, --libdir, and --bindir options.\n"
+                print ("\nNeed to define CODA, or use the --prefix option,")
+                print ("or all the --incdir, --libdir, and --bindir options.\n")
                 raise SystemExit
         else:
             prefix = codaHomeEnv
-    
+
     osDir = prefix + "/" + osname
-    
+
     # Set our install directories
     if incdir != None:
         incDir = incdir
@@ -168,17 +168,17 @@ def getInstallationDirs(osname, prefix, incdir, libdir, bindir):
     else:
         incDir = prefix + '/common/include'
         archIncDir = osDir + '/include'
-    
+
     if libdir != None:
         libDir = libdir
     else:
         libDir = osDir + '/lib'
-    
+
     if bindir != None:
         binDir = bindir
     else:
         binDir = osDir + '/bin'
-    
+
     # Return absolute paths in list
     return [os.path.abspath(prefix),
             os.path.abspath(osDir),
@@ -203,7 +203,7 @@ def makeIncludeDirs(includeDir, archIncludeDir, archDir, archIncLocalLink):
     # Make sure it's a directory (if we didn't create it)
     elif not os.path.isdir(includeDir):
         print
-        print "Error:", includeDir, "is NOT a directory"
+        print ("Error:", includeDir, "is NOT a directory")
         print
         raise SystemExit
 
@@ -221,7 +221,7 @@ def makeIncludeDirs(includeDir, archIncludeDir, archDir, archIncLocalLink):
             return
     elif not os.path.isdir(archDir):
         print
-        print "Error:", archDir, "is NOT a directory"
+        print ("Error:", archDir, "is NOT a directory")
         print
         raise SystemExit
 
@@ -232,24 +232,24 @@ def makeIncludeDirs(includeDir, archIncludeDir, archDir, archIncLocalLink):
     if not os.path.exists(archIncludeDir):
         # Create symbolic link: symlink(source, linkname)
         try:
-    	    if (archIncLocalLink == None) or (archIncLocalLink == ''):
-	    	symlink(includeDir, archIncludeDir)
+            if (archIncLocalLink == None) or (archIncLocalLink == ''):
+                symlink(includeDir, archIncludeDir)
             else:
-	    	symlink(archIncLocalLink, archIncludeDir)
+                symlink(archIncLocalLink, archIncludeDir)
         except OSError:
             # Failed to create symbolic link, so
             # just make it a regular directory
             os.makedirs(archIncludeDir)
     elif not os.path.isdir(archIncludeDir):
         print
-        print "Error:", archIncludeDir, "is NOT a directory"
+        print ("Error:", archIncludeDir, "is NOT a directory")
         print
         raise SystemExit
 
     return
 
 
- 
+
 ###########
 # JAVA JNI
 ###########
@@ -266,19 +266,19 @@ def configureJNI(env):
             java_base = '/System/Library/Frameworks/JavaVM.framework'
         else:
             # Search for the java compiler
-            print "JAVA_HOME environment variable not set. Searching for javac to find jni.h ..."
+            print ("JAVA_HOME environment variable not set. Searching for javac to find jni.h ...")
             if not env.get('JAVAC'):
-                print "The Java compiler must be installed and in the current path, exiting"
+                print ("The Java compiler must be installed and in the current path, exiting")
                 return 0
             jcdir = os.path.dirname(env.WhereIs('javac'))
             if not jcdir:
-                print "   not found, exiting"
+                print ("   not found, exiting")
                 return 0
             # assuming the compiler found is in some directory like
             # /usr/jdkX.X/bin/javac, java's home directory is /usr/jdkX.X
             java_base = os.path.join(jcdir, "..")
-            print "  found, dir = " + java_base        
-        
+            print ("  found, dir = " + java_base)
+
     if sys.platform == 'darwin':
         # Apple does not use Sun's naming convention
         java_headers = [os.path.join(java_base, 'Headers')]
@@ -332,41 +332,41 @@ def generateDocs(env, doC=False, doCPP=False, doJava=False, javaDir=''):
             cmd = 'doxygen doc/doxygen/DoxyfileC'
             pipe = Popen(cmd, shell=True, env={"TOPLEVEL": "./"}, stdout=PIPE).stdout
             return
-            
+
         # Doc files builders
         docBuildC = Builder(action = docGeneratorC)
         env.Append(BUILDERS = {'DocGenC' : docBuildC})
-        
+
         # Generate documentation
         env.Alias('doc', env.DocGenC(target = ['#/doc/doxygen/C/html/index.html'],
               source = scanFiles("src/libsrc", accept=["*.[ch]"]) ))
-    
-    
+
+
     if doCPP:
         def docGeneratorCC(target, source, env):
             cmd = 'doxygen doc/doxygen/DoxyfileCC'
             pipe = Popen(cmd, shell=True, env={"TOPLEVEL": "./"}, stdout=PIPE).stdout
             return
-        
+
         docBuildCC = Builder(action = docGeneratorCC)
         env.Append(BUILDERS = {'DocGenCC' : docBuildCC})
-        
+
         env.Alias('doc', env.DocGenCC(target = ['#/doc/doxygen/CC/html/index.html'],
               source = scanFiles("src/libsrc++", accept=["*.[ch]", "*.cc", "*.hxx"]) ))
-    
-    
+
+
     if doJava:
         def docGeneratorJava(target, source, env):
             cmd = 'ant javadoc'
             output = os.popen(cmd).read()
             return
-        
+
         docBuildJava = Builder(action = docGeneratorJava)
         env.Append(BUILDERS = {'DocGenJava' : docBuildJava})
-        
+
         env.Alias('doc', env.DocGenJava(target = ['#/doc/javadoc/index.html'],
             source = scanFiles(javaDir, accept=["*.java"]) ))
-    
+
     return 1
 
 
@@ -379,13 +379,13 @@ def removeDocs(env):
         cmd = 'rm -fr doc/javadoc doc/doxygen/C doc/doxygen/CC'
         output = os.popen(cmd).read()
         return
-    
+
     docRemoveAll = Builder(action = docRemover)
     env.Append(BUILDERS = {'DocRemove' : docRemoveAll})
-    
+
     # remove documentation
     env.Alias('undoc', env.DocRemove(target = ['#/.undoc'], source = None))
-    
+
     return 1
 
 
@@ -396,7 +396,7 @@ def removeDocs(env):
 
 def generateTarFile(env, baseName, majorVersion, minorVersion):
     """Generate a gzipped tar file of the current directory."""
-                
+
     # Function that does the tar. Note that tar on Solaris is different
     # (more primitive) than tar on Linux and MacOS. Solaris tar has no -z option
     # and the exclude file does not allow wildcards. Thus, stick to Linux for
